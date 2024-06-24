dae8f1e9319803649ecaf11b0ba9a68708956f42
unknown <alex.naziru.dev@gmail.com>
2024-06-24 16:32:54 +0300

    responsive

--- includes/nav.php
+++ includes/nav.php
@@ -1,5 +1,5 @@
- <nav class="navbar navbar-fixed-top">
-        <div class="container">
+ <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container-fluid">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
                     <span class="sr-only">Toggle navigation</span>
@@ -11,7 +11,7 @@
             </div>
             <div id="navbar" class="collapse navbar-collapse">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="/<?php echo $root_directory ?>/index.php">Home</a></li>
+                    <li><a href="/<?php echo $root_directory ?>/index.php">Home</a></li>
                     <li><a href="/<?php echo $root_directory ?>/page1.php">Page 1</a></li>
                     <li><a href="/<?php echo $root_directory ?>/page2.php">Page 2</a></li>
                     <li><a href="/<?php echo $root_directory ?>/page3.php">Page 3</a></li>
